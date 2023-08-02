0084352c14c0432acd32f2894b76731cde1f26f7
vincentdao <vincentdaomain@gmail.com>
2023-08-02 12:42:04 +0700

    init spring boot

---
+++ pim-tool-back/src/test/java/com/vincentdao/pimtoolback/PimToolBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincentdao.pimtoolback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimToolBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
